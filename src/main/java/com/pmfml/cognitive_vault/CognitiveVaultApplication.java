90df9cc8e35fe66dbe05f237f2c8a946a6d4197a
Paulo Leite <foxmodem@hotmail.com>
2026-06-19 21:15:24 -0300

    chore: initial commit boilerplate

---
+++ src/main/java/com/pmfml/cognitive_vault/CognitiveVaultApplication.java
@@ -0,0 +1,13 @@
+package com.pmfml.cognitive_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitiveVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmfml/cognitive_vault/CognitiveVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmfml.cognitive_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitiveVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
